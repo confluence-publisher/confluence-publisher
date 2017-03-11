2fa639afd3efde02e0b11f7443731ee9bc5591fe
cstettler <cstettler@imap.ch>
2017-03-11 13:30:08 +0100

    adjusted docker logs to send output to stdout in order for grep to catch it

--- waitForConfluenceStartup.sh
+++ waitForConfluenceStartup.sh
@@ -3,4 +3,4 @@
 containerName=$1
 timeoutInSeconds=$2
 
-timeout ${timeoutInSeconds} grep -q "org.apache.catalina.startup.Catalina.start Server startup in" <(docker logs -f ${containerName})
+timeout ${timeoutInSeconds} grep -q "org.apache.catalina.startup.Catalina.start Server startup in" <(docker logs -f ${containerName} 2>&1)

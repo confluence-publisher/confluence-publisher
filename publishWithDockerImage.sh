378791133bb399ccb0be42ae2b72f4826d23fa66
cstettler <cstettler@imap.ch>
2018-04-22 13:58:22 +0200

    fixed permissions of test script (must be executable)

--- publishWithDockerImage.sh
+++ publishWithDockerImage.sh
@@ -7,8 +7,8 @@ docker run --rm -e ROOT_CONFLUENCE_URL=http://$runnerHost:8090 \
     -e USERNAME=confluence-publisher-it \
     -e PASSWORD=1234 \
     -e SPACE_KEY=CPI \
-    -e ANCESTOR_ID=327706
+    -e ANCESTOR_ID=327706 \
     -e PAGE_TITLE_PREFIX="Docker - " \
-    -e PAGE_TITLE_SUFFIX=" - Test"
+    -e PAGE_TITLE_SUFFIX=" - Test" \
     -v `pwd`/asciidoc-confluence-publisher-doc/etc/docs:/var/asciidoc-root-folder \
     confluencepublisher/confluence-publisher:$projectVersion

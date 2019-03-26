f65f8de48b1c5ad168c66f24817aec2abc3cad82
cstettler <cstettler@imap.ch>
2019-03-26 22:56:10 +0100

    streamlined arguments parser to avoid static state

--- asciidoc-confluence-publisher-cli/src/main/java/org/sahli/asciidoc/confluence/publisher/cli/ArgumentsParser.java
+++ asciidoc-confluence-publisher-cli/src/main/java/org/sahli/asciidoc/confluence/publisher/cli/ArgumentsParser.java
@@ -3,40 +3,48 @@ package org.sahli.asciidoc.confluence.publisher.cli;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 import static java.util.Arrays.stream;
 
-public class ArgumentsParser {
+class ArgumentsParser {
 
-    private static final TypeReference<HashMap<String, Object>> typeRef =
-            new TypeReference<HashMap<String, Object>>() {
-            };
-    private static final ObjectMapper objectMapper = new ObjectMapper();
+    private final TypeReference<Map<String, Object>> typeReference;
+    private final ObjectMapper objectMapper;
 
-    public Optional<Map<String, Object>> optionalJsonArgument(String key, String[] args) {
+    ArgumentsParser() {
+        this.typeReference = mapOfStringToObjectTypeReference();
+        this.objectMapper = new ObjectMapper();
+    }
+
+    Optional<Map<String, Object>> optionalJsonArgument(String key, String[] args) {
         return optionalArgument(key, args)
                 .map(attributes -> {
                     try {
-                        return objectMapper.readValue(attributes, typeRef);
+                        return this.objectMapper.readValue(attributes, this.typeReference);
                     } catch (Exception e) {
                         throw new IllegalArgumentException("failed to parse argument '" + key + "' to map due to error", e);
                     }
                 });
     }
 
-    public String mandatoryArgument(String key, String[] args) {
+    String mandatoryArgument(String key, String[] args) {
         return optionalArgument(key, args)
                 .orElseThrow(() -> new IllegalArgumentException("mandatory argument '" + key + "' is missing"));
     }
 
-    public Optional<String> optionalArgument(String key, String[] args) {
+    Optional<String> optionalArgument(String key, String[] args) {
         return stream(args)
                 .filter((keyAndValue) -> keyAndValue.startsWith(key + "="))
                 .map((keyAndValue) -> keyAndValue.substring(keyAndValue.indexOf('=') + 1))
                 .filter((value) -> !value.isEmpty())
                 .findFirst();
     }
+
+    private static TypeReference<Map<String, Object>> mapOfStringToObjectTypeReference() {
+        return new TypeReference<Map<String, Object>>() {
+        };
+    }
+
 }

cb95b10b5e2464f29535dd33d6f474e8f70d5446
cstettler <cstettler@imap.ch>
2020-07-27 15:58:55 +0200

    fixed maven version expression pattern to support multi-digit version number segments

--- publishWithDockerImage.sh
+++ publishWithDockerImage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-projectVersion=`./mvnw org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -e "^[0-9]\.[0-9]\.[0-9].*" 2>&1`
+projectVersion=`./mvnw org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -e "^[0-9]\+\.[0-9]\+\.[0-9]\+.*" 2>&1`
 
 docker run --network container:confluence-publisher-it --rm \
     -e ROOT_CONFLUENCE_URL=http://localhost:8090 \

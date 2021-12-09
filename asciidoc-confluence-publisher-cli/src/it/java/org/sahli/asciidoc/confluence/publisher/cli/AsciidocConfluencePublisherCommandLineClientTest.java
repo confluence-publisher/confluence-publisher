f65e0b42a25b80484b2782e839eb64e67005865b
Bartosz Jablonski-Adamczyk <Bartosz.Jablonski-Adamczyk@stepstone.com>
2021-12-09 09:35:14 +0100

    Make username optional in command line client

--- asciidoc-confluence-publisher-cli/src/it/java/org/sahli/asciidoc/confluence/publisher/cli/AsciidocConfluencePublisherCommandLineClientTest.java
+++ asciidoc-confluence-publisher-cli/src/it/java/org/sahli/asciidoc/confluence/publisher/cli/AsciidocConfluencePublisherCommandLineClientTest.java
@@ -68,7 +68,6 @@ public class AsciidocConfluencePublisherCommandLineClientTest {
                 "ancestorId",
                 "spaceKey",
                 "password",
-                "username",
                 "rootConfluenceUrl",
                 "asciidocRootFolder"
         );

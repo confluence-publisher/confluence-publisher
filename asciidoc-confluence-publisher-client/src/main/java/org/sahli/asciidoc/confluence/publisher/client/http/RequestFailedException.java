ad97432d51a4878d7b24bd52df045878fc132409
cstettler <cstettler@imap.ch>
2022-03-10 15:11:50 +0100

    ensured error reason returned from confluence is shown more prominent in error log

--- asciidoc-confluence-publisher-client/src/main/java/org/sahli/asciidoc/confluence/publisher/client/http/RequestFailedException.java
+++ asciidoc-confluence-publisher-client/src/main/java/org/sahli/asciidoc/confluence/publisher/client/http/RequestFailedException.java
@@ -16,6 +16,9 @@
 
 package org.sahli.asciidoc.confluence.publisher.client.http;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
 import org.apache.http.HttpRequest;
@@ -36,29 +39,44 @@ import static org.sahli.asciidoc.confluence.publisher.client.utils.InputStreamUt
 public class RequestFailedException extends RuntimeException {
 
     RequestFailedException(HttpRequest request, HttpResponse response, Exception reason) {
-        super("request failed (" +
-                "request: " + requestLog(request) + ", " +
-                "response: " + responseLog(response) +
-                (reason != null ? ", reason: '" + reason.getMessage() + "'" : "") +
-                ")", reason);
+        super(buildMessage(request, response, reason), reason);
     }
 
-    private static String requestLog(HttpRequest request) {
-        String requestLine = request.getRequestLine().getMethod() + " " + request.getRequestLine().getUri();
+    private static String buildMessage(HttpRequest request, HttpResponse response, Exception reason) {
         String requestBody = requestBody(request);
+        String responseBody = response != null ? responseBody(response) : "<none>";
 
-        return requestLine + " " + requestBody;
+        String requestLog = statusLine(request) + " " + requestBody;
+        String responseLog = response != null ? statusLine(response) + " " + responseBody : responseBody;
+        String reasonLog = reason != null ? reason.getMessage() : "<none>";
+
+        return reasonFromResponseOrDefault(responseBody, "request failed") + " (" +
+                "request: " + requestLog + ", " +
+                "response: " + responseLog + ", " +
+                "reason: " + reasonLog +
+                ")";
     }
 
-    private static String responseLog(HttpResponse response) {
-        if (response == null) {
-            return "<none>";
+    private static String reasonFromResponseOrDefault(String responseBody, String defaultReason) {
+        try {
+            JsonNode responseNode = new ObjectMapper().readTree(responseBody);
+            JsonNode messageNode = responseNode.get("message");
+
+            if (messageNode != null && messageNode.asText() != null) {
+                return messageNode.asText();
+            }
+        } catch (JsonProcessingException ignored) {
         }
 
-        String statusLine = response.getStatusLine().getStatusCode() + " " + response.getStatusLine().getReasonPhrase();
-        String responseBody = responseBody(response);
+        return defaultReason;
+    }
 
-        return statusLine + " " + responseBody;
+    private static String statusLine(HttpRequest request) {
+        return request.getRequestLine().getMethod() + " " + request.getRequestLine().getUri();
+    }
+
+    private static String statusLine(HttpResponse response) {
+        return response.getStatusLine().getStatusCode() + " " + response.getStatusLine().getReasonPhrase();
     }
 
     private static String requestBody(HttpRequest request) {
---
+++ asciidoc-confluence-publisher-client/src/test/java/org/sahli/asciidoc/confluence/publisher/client/http/RequestFailedExceptionTest.java
@@ -0,0 +1,249 @@
+/*
+ * Copyright 2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.sahli.asciidoc.confluence.publisher.client.http;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpEntityEnclosingRequest;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.RequestLine;
+import org.apache.http.StatusLine;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class RequestFailedExceptionTest {
+
+    @Test
+    public void getMessage_exceptionWithGetRequestAndResponse_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("GET", "http://localhost/test", null);
+        HttpResponse response = mockResponse(400, "bad request", null);
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, null);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("request failed (" +
+                "request: GET http://localhost/test <empty body>, " +
+                "response: 400 bad request <empty body>, " +
+                "reason: <none>" +
+                ")")));
+    }
+
+    @Test
+    public void getMessage_exceptionWithPostRequestAndResponse_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("POST", "http://localhost/test", "request body");
+        HttpResponse response = mockResponse(400, "bad request", null);
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, null);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("request failed (" +
+                "request: POST http://localhost/test request body, " +
+                "response: 400 bad request <empty body>, " +
+                "reason: <none>" +
+                ")")));
+    }
+
+    @Test
+    public void getMessage_exceptionWithResponseWithJsonBodyWithMessage_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("GET", "http://localhost/test", null);
+        HttpResponse response = mockResponse(400, "bad request", "{\"message\": \"reason in response\"}");
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, null);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("reason in response (" +
+                "request: GET http://localhost/test <empty body>, " +
+                "response: 400 bad request {\"message\": \"reason in response\"}, " +
+                "reason: <none>" +
+                ")")));
+    }
+
+    @Test
+    public void getMessage_exceptionWithResponseWithJsonBodyWithoutMessage_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("GET", "http://localhost/test", null);
+        HttpResponse response = mockResponse(400, "bad request", "{\"some\": \"json\"}");
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, null);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("request failed (" +
+                "request: GET http://localhost/test <empty body>, " +
+                "response: 400 bad request {\"some\": \"json\"}, " +
+                "reason: <none>" +
+                ")")));
+    }
+
+    @Test
+    public void getMessage_exceptionWithResponseWithNonJsonBody_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("GET", "http://localhost/test", null);
+        HttpResponse response = mockResponse(400, "bad request", "response body");
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, null);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("request failed (" +
+                "request: GET http://localhost/test <empty body>, " +
+                "response: 400 bad request response body, " +
+                "reason: <none>" +
+                ")")));
+    }
+
+    @Test
+    public void getMessage_exceptionWithNullResponse_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("GET", "http://localhost/test", null);
+        HttpResponse response = null;
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, null);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("request failed (" +
+                "request: GET http://localhost/test <empty body>, " +
+                "response: <none>, " +
+                "reason: <none>" +
+                ")")));
+    }
+
+    @Test
+    public void getMessage_exceptionWithReason_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("GET", "http://localhost/test", null);
+        HttpResponse response = null;
+        Exception reason = new Exception("reason from exception");
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, reason);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("request failed (" +
+                "request: GET http://localhost/test <empty body>, " +
+                "response: <none>, " +
+                "reason: reason from exception" +
+                ")")));
+    }
+
+    @Test
+    public void getMessage_exceptionWithoutReason_returnsCorrectMessage() {
+        // arrange
+        HttpRequest request = mockRequest("GET", "http://localhost/test", null);
+        HttpResponse response = null;
+        Exception reason = null;
+
+        RequestFailedException requestFailedException = new RequestFailedException(request, response, reason);
+
+        // act
+        String message = requestFailedException.getMessage();
+
+        // assert
+        assertThat(message, is(equalTo("request failed (" +
+                "request: GET http://localhost/test <empty body>, " +
+                "response: <none>, " +
+                "reason: <none>" +
+                ")")));
+    }
+
+    private static HttpRequest mockRequest(String method, String uri, String requestBody) {
+        if (requestBody != null) {
+            RequestLine requestLine = mockRequestLine(method, uri);
+            HttpEntity httpEntity = mockHttpEntity(requestBody);
+
+            HttpEntityEnclosingRequest request = mock(HttpEntityEnclosingRequest.class);
+            when(request.getRequestLine()).thenReturn(requestLine);
+            when(request.getEntity()).thenReturn(httpEntity);
+
+            return request;
+        } else {
+            RequestLine requestLine = mockRequestLine(method, uri);
+
+            HttpRequest request = mock(HttpRequest.class);
+            when(request.getRequestLine()).thenReturn(requestLine);
+
+            return request;
+        }
+    }
+
+    private static HttpResponse mockResponse(int statusCode, String statusReason, String responseBody) {
+        StatusLine statusLine = mockStatusLine(statusCode, statusReason);
+        HttpEntity httpEntity = responseBody != null ? mockHttpEntity(responseBody) : null;
+
+        HttpResponse response = mock(HttpResponse.class);
+        when(response.getStatusLine()).thenReturn(statusLine);
+        when(response.getEntity()).thenReturn(httpEntity);
+
+        return response;
+    }
+
+    private static RequestLine mockRequestLine(String method, String uri) {
+        RequestLine requestLine = mock(RequestLine.class);
+        when(requestLine.getMethod()).thenReturn(method);
+        when(requestLine.getUri()).thenReturn(uri);
+
+        return requestLine;
+    }
+
+    private static StatusLine mockStatusLine(int statusCode, String statusReason) {
+        StatusLine statusLine = mock(StatusLine.class);
+        when(statusLine.getStatusCode()).thenReturn(statusCode);
+        when(statusLine.getReasonPhrase()).thenReturn(statusReason);
+
+        return statusLine;
+    }
+
+    private static HttpEntity mockHttpEntity(String body) {
+        try {
+            HttpEntity httpEntity = mock(HttpEntity.class);
+            when(httpEntity.getContent()).thenReturn(new ByteArrayInputStream(body.getBytes()));
+
+            return httpEntity;
+        } catch (IOException e) {
+            throw new IllegalStateException("unable to mock http entity", e);
+        }
+    }
+}
